1c19344b8081eab12cecdedb232f4dbb1ccea3eb
= <=>
2017-01-28 16:21:51 -0700

    the snake is now displayed and moving

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import snake.snakeBase as Snake
+import breakout.Breakout as breakout
+from pygame.locals import *
 import pygame
 
+
+
 pygame.init()
-surface = pygame.display.set_mode((600,700))
+surface = pygame.display.set_mode((800,700))
 Snake.init(pygame,surface)
-snake = Snake.Snake(1,0,[50,50])
+snake = Snake.Snake(3,0,[50,50])
+
+def handle_events():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            return True
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                snake.up()
+            elif event.key == K_DOWN:
+                snake.down()
+            elif event.key == K_LEFT:
+                snake.left()
+            elif event.key == K_RIGHT:
+                snake.right()
+
 while True:
     snake.update()
+    pygame.display.update()
+    if handle_events(): break;
--- snake/snakeBase.py
+++ snake/snakeBase.py
@@ -1,19 +1,35 @@
 from collections import deque
 from time import time
-pixel_size = 5
+pixel_size = 10
 pixel = (pixel_size,pixel_size)
 
-def init(pgame,surface_):
-    Snake.pygame = pgame
-    Snake.surface = surface_
-    print('pygame initialised')
 
+
+class init:
+    pygame = None
+    surface = None
+    Tail = None
+    def __init__(self,pygame,surface):
+        init.pygame = pygame
+        init.surface = surface
+        class Tail(pygame.Rect):
+            color = 0xFFFFFF
+            def __init__(self,pos):
+                self.pos = pos
+                self.x = pos[0]
+                self.y = pos[1]
+                self.size = pixel
+                
+            def erase(self):
+                init.pygame.draw.rect(init.surface,0x000000, self)
+                
+            def draw(self):
+                init.pygame.draw.rect(init.surface,self.color, self)
+        init.Tail = Tail 
 
 class Snake:
 
     color = 0x5555FF
-    pygame = None
-    surface = None
     def __init__(self,start_size,growth,pos):
         self.tail = deque()
         self.dir = 1
@@ -25,40 +41,41 @@ class Snake:
             self.size_up()
 
     def size_up(self):
-        self.tail.append(Tail(self.pos))
+        self.tail.append(init.Tail(self.pos))
 
     def left(self):
-        if not self.changed:
-            self.dir = 1
-            self.changed = True
-
-    def right(self):
-        if not self.changed:
+        if not self.changed and self.dir != 1:
             self.dir = 0
             self.changed = True
 
+    def right(self):
+        if not self.changed and self.dir != 0:
+            self.dir = 1
+            self.changed = True
+
     def up(self):
-        if not self.changed:
-            self.dir = 3
+        if not self.changed and self.dir != 3:
+            self.dir = 2
             self.changed = True
 
     def down(self):
-        if not self.changed:
-            self.dir = 2
+        if not self.changed and self.dir != 2:
+            self.dir = 3
             self.changed = True
 
     def __move(self):
         move = pixel_size
         if self.dir % 2: pass  # move *=1
         else: move *= -1
-        if self.dir// 2: pos[1] += move
+        if self.dir// 2: self.pos[1] += move
         else: self.pos[0] += move
+        self.changed = False
         
     def update(self):
         if time() >= self.refresh:
             self.tail[-1].erase()
             self.tail.pop()
-            self.tail.appendleft(Tail(self.pos))
+            self.tail.appendleft(init.Tail(self.pos))
             self.tail[0].draw()
             self.__move()
             self.__draw()
@@ -66,15 +83,7 @@ class Snake:
         #self.hi
         
     def __draw(self):
-        self.pygame.draw.rect(Snake.surface,self.color, self.pygame.Rect(pos,pixel))
+        init.pygame.draw.rect(init.surface,self.color, init.pygame.Rect(self.pos,pixel))
 
-class Tail:
-    color = 0xFFFFFF
-    def __init__(self,pos):
-        self.pos = pos
-        
-    def erase(self):
-        Snake.pygame.draw.rect(Snake.surface,0x000000, Snake.pygame.Rect(self.pos,pixel))
-        
-    def draw(self):
-        Snake.pygame.draw.rect(Snake.surface,self.color, Snake.pygame.Rect(self.pos,pixel))
+
+
